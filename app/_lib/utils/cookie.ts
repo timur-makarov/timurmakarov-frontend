965fdc5417e2bf294aa335d875e5dfea08e5d553
Timur Makarov <makarov-timyr@ya.ru>
2024-06-10 17:18:01 +0400

    fix: change COOKIE_DOMAIN

--- app/_lib/utils/cookie.ts
+++ app/_lib/utils/cookie.ts
@@ -15,11 +15,11 @@ export function getCookie(name: string): string | null {
 
 export function setCookie(name: string, value: string, days: number) {
   let expires = ''
-  if (days) {
-    const date = new Date()
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
-    expires = `; expires=${date.toUTCString()}`
-  }
+
+  const date = new Date()
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+  expires = `; expires=${date.toUTCString()}`
+
   const domain = process.env.NEXT_PUBLIC_COOKIE_DOMAIN || 'localhost'
   document.cookie = `${name}=${value || ''}${expires}; path=/; domain=${domain}`
 }
