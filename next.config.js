ac15b2e1902d00275423e59bc26303dea9a7fa86
Timur Makarov <makarov-timyr@ya.ru>
2024-07-15 17:08:38 +0400

    chore: add domains in next-config back

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    domains: ['cms.timurmakarov.com', 'localhost'],
     remotePatterns: [
       {
         protocol: 'https',
